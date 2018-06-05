0ecb0075402a8c24a9b48b2a2f58a34b496d7556
rahulramanathan <rrahul1997@gmail.com>
2018-06-05 16:09:32 +0530

    Fixed broken URLS

--- myproject/settings/dev.py
+++ myproject/settings/dev.py
@@ -1,4 +1,5 @@
 from myproject.settings.base import *
+
 DEBUG = True
 if 'myapp' not in INSTALLED_APPS:
     INSTALLED_APPS.append('myapp')
--- myproject/settings/prod.py
+++ myproject/settings/prod.py
@@ -1,5 +1,7 @@
 from myproject.settings.base import *
+from myproject.urls import urlpatterns
+#if 'myapp' in INSTALLED_APPS:
+#    INSTALLED_APPS.remove('myapp')
+urlpatterns.remove('accounts/')
 DEBUG = False
 ALLOWED_HOSTS = ['*']
-if 'myapp' in INSTALLED_APPS:
-    INSTALLED_APPS.remove('myapp')
